260f157a685a126aed627c2f5a38e006cad9cde0
5um4n7h <sumanthhegde13@gmail.com>
2023-03-06 23:17:54 +0530

    updated

--- file.cmd
+++ file.cmd
@@ -4,3 +4,10 @@ Title Windows Optimizer
 echo "Disabling Windows tips and tricks"
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSyncProviderNotifications /t REG_DWORD /d 0 /f
 pause
+set username=%USERNAME%
+echo Current logged in user: %username%
+set tempdir=C:\Users\%username%\AppData\Local\Temp
+echo Deleting directory: %tempdir%
+rmdir /s /q %tempdir%
+echo Done.
+pause
