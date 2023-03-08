d6c2c2cfbca9fd2a36be5d9aa2bd9d577ceec8ff
5um4n7h <sumanthhegde13@gmail.com>
2023-03-08 21:49:55 +0530

    UAC prompt added

--- file.cmd
+++ file.cmd
@@ -1,13 +1,29 @@
 ::in progress
 @echo off
 Title Windows Optimizer
-echo "Disabling Windows tips and tricks"
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSyncProviderNotifications /t REG_DWORD /d 0 /f
-pause
+:: Check if script is running with admin privileges
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :continue
+) else (
+    :: Prompt the user to run the batch file as an administrator using the UAC prompt
+    echo This script requires administrator privileges. Please click "Yes" in the User Account Control prompt to continue.
+    powershell -Command "Start-Process '%~dpnx0' -Verb runAs"
+    exit /b %errorlevel%
+)
+
+:continue
 set username=%USERNAME%
 echo Current logged in user: %username%
 set tempdir=C:\Users\%username%\AppData\Local\Temp
+echo Taking ownership of directory: %tempdir%
+takeown /f "%tempdir%" /r /d y
+echo Granting full control permissions to directory: %tempdir%
+icacls "%tempdir%" /grant "%username%":(F) /t
 echo Deleting directory: %tempdir%
-rmdir /s /q %tempdir%
+RD /S /Q "%tempdir%"
 echo Done.
+net stop WSearch
+sc config WSearch start=disabled
 pause
+
