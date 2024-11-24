854b51ac0eb9380d43e11fd05de18a3baf00e001
Sumanth Hegde <sumanthhegde13@gmail.com>
2024-11-24 20:02:48 +0530

    files added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String username = System.getProperty("user.name");
+        System.out.println("Current user: " + username);
+
+
+    }
+}
